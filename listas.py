993b5c751325a442ff52fbe743d2f9a7dbeff70f
Phnick <115507431+Phnick@users.noreply.github.com>
2024-05-06 18:49:54 -0300

    Add files via upload
    Aprendendo a usar listas em Python

---
+++ listas.py
@@ -0,0 +1,84 @@
+lista = ['Pedro', 9.0, 8.0, 7.0, True]
+
+for elemento in lista:
+    print(elemento)
+
+lista[3]=10
+print(lista)    
+
+
+
+
+# como saber a quantidades de elemenstos na lista len()
+lista2 =[1,2,3,4,8]
+print(lista2)
+
+
+
+tamanho =len(lista2)
+print(tamanho)
+
+
+# Partição  de lista 
+lista =[1.3,2.5,3.5,4.8,8.9]
+media = (lista[0] + lista[1] + lista[2] + lista[3] + lista[4])/4
+print(media)
+print(lista)
+
+quebra= lista[:4]
+print(quebra)
+
+
+# Método append() ele adiciona um elemneto na lista
+numero = [1,2,3]
+numero.append(7)
+print(numero)
+
+# Método extend() adiciona vários elmentos na lista
+
+numero = [1,2,3]
+numero.extend([8,9,10])
+print(numero)
+
+
+# Método remove() remove um elemento específico da lista
+numero = [1,2,3]
+numero.remove(1)
+print(numero)
+
+# Método insert() isere o elemento em uma determinada posição da lista
+
+animais = ['Cachorro','Gato','Rato']
+print(animais)
+animais.insert(2,'Macaco')
+print(animais)
+
+
+# Método pop() remove o elemento de uma determinada posição da lista
+animais = ['Cachorro','Gato','Rato']
+print(animais)
+animais.pop(0)
+print(animais)
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+    
+
+
+
+ 
+    
+    
+
+
